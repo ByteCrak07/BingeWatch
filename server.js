253b2909c98f71e28b2eedff4e5ccd414d025177
Abil Savio <abilsavio@gmail.com>
2021-08-13 00:23:39 +0530

    Add heroku config

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to Binge Watch🍿 Api!!");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
