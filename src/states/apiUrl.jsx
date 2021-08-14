e51687557cc9d18ee097043a3034b14413b7fb9e
Abil Savio <abilsavio@gmail.com>
2021-08-14 20:08:22 +0530

    Change url

--- src/states/apiUrl.jsx
+++ src/states/apiUrl.jsx
@@ -1,4 +1,4 @@
-// const apiUrl = "https://binge-watch-api.herokuapp.com";
-const apiUrl = "http://localhost:5000";
+const apiUrl = "https://binge-watch-api.herokuapp.com";
+// const apiUrl = "http://localhost:5000";
 
 export default apiUrl;
