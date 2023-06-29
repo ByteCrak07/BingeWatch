81e74aa73e6396687fa91539fa4627bbb5b965f0
Abil Savio <abilsavio@gmail.com>
2023-06-29 15:19:39 +0530

    Update backend url

--- src/states/apiUrl.jsx
+++ src/states/apiUrl.jsx
@@ -1,4 +1,4 @@
-const apiUrl = "https://binge-watch-api.herokuapp.com";
+const apiUrl = "https://bingewatch-backend.onrender.com";
 // const apiUrl = "http://localhost:5000";
 
 export default apiUrl;
